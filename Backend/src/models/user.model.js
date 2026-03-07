538de00706f9332fc53d82e8c8a5630564779643
akdrin <akdr9956@gmail.com>
2026-03-07 14:43:02 +0530

    initiated db models

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,21 +1,51 @@
 const mongoose=require('mongoose')
 
 const userSchema=new mongoose.Schema({
-    username:{
-        type: String,
-        unique: [true,"Username already exist"],
-        required: true,
+    name:{
+        type:String,
+        required: true
     },
-    email:{
+    role:{
         type: String,
-        unique: [true,"Email already exist"],
-        required: true,
+        enum: ["Sanjivni Sahayak", "Doctor"],
+        required: true
+    },
+    uniqueID:{
+        type: String,
+        unique: [true,"Already Registered"],
+        required:true
     },
     password:{
         type: String,
         required: true
+    },
+    state:{
+        type: String,
+        required: true
+    }, // used to validate uniqueId
+    verified:{
+        type: Boolean,
+        default: false
+    },
+    email:{
+        type: String,
+        unique: [true,"Email already exist"],
+        required: true
+    },
+    phone:{
+        type: String,
+        unique: [true,"Phone Number already exist"],
+        required: true
+    },
+    clinicName:{
+        type:String,
+        required: true
+    },
+    address:{
+        type:String,
+        required: true
     }
-})
+},{timestamps: true})
 
 const userModel = mongoose.model("users",userSchema)
 
